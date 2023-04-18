65f1273a504c80e6769c008618d207dd62ab29a4
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-04-18 05:27:46 +0000

    Add ChatGPT Demo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TempCodespaces
 Temp workspace for Codespaces
+
+
+ChatGPT generate slides demo
+```
+Create a slides outline for "Develop the ChatGPT VR Chatbot", and 
+create a python script that generates a powerpoint file based on that outline in point form. Use the pptx library.
+```
---
+++ slide_generator.py
@@ -0,0 +1,76 @@
+import collections.abc  
+import pptx
+
+# create new powerpoint file
+prs = pptx.Presentation()
+
+# add title slide
+title_slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(title_slide_layout)
+title = slide.shapes.title
+subtitle = slide.placeholders[1]
+title.text = "Develop the ChatGPT VR Chatbot"
+subtitle.text = "By AI Assistant"
+
+# add introduction slide
+intro_slide_layout = prs.slide_layouts[1]
+slide = prs.slides.add_slide(intro_slide_layout)
+title = slide.shapes.title
+title.text = "Introduction"
+
+# add bullet points
+bullet_points = slide.shapes.placeholders[1]
+bullet_points.text = "What is ChatGPT?\nWhy develop a VR Chatbot?"
+
+# add understanding slide
+understanding_slide_layout = prs.slide_layouts[1]
+slide = prs.slides.add_slide(understanding_slide_layout)
+title = slide.shapes.title
+title.text = "Understanding VR and Chatbots"
+
+# add bullet points
+bullet_points = slide.shapes.placeholders[1]
+bullet_points.text = "What is VR?\nWhat are Chatbots?\nWhy combine them?"
+
+# add developing slide
+developing_slide_layout = prs.slide_layouts[1]
+slide = prs.slides.add_slide(developing_slide_layout)
+title = slide.shapes.title
+title.text = "Developing the ChatGPT VR Chatbot"
+
+# add bullet points
+bullet_points = slide.shapes.placeholders[1]
+bullet_points.text = "Choosing a platform\nGathering data for training the model\nBuilding the Chatbot\nIntegrating with VR"
+
+# add benefits slide
+benefits_slide_layout = prs.slide_layouts[1]
+slide = prs.slides.add_slide(benefits_slide_layout)
+title = slide.shapes.title
+title.text = "Benefits of ChatGPT VR Chatbot"
+
+# add bullet points
+bullet_points = slide.shapes.placeholders[1]
+bullet_points.text = "Enhanced user experience\nIncreased engagement\nImproved customer service\nPotential for data collection and analysis"
+
+# add challenges slide
+challenges_slide_layout = prs.slide_layouts[1]
+slide = prs.slides.add_slide(challenges_slide_layout)
+title = slide.shapes.title
+title.text = "Challenges and Limitations"
+
+# add bullet points
+bullet_points = slide.shapes.placeholders[1]
+bullet_points.text = "Technical limitations\nUser acceptance\nEthical considerations"
+
+# add conclusion slide
+conclusion_slide_layout = prs.slide_layouts[1]
+slide = prs.slides.add_slide(conclusion_slide_layout)
+title = slide.shapes.title
+title.text = "Conclusion"
+
+# add bullet points
+bullet_points = slide.shapes.placeholders[1]
+bullet_points.text = "Recap of benefits and limitations\nFuture developments"
+
+# save powerpoint file
+prs.save("ChatGPT VR Chatbot.pptx")
